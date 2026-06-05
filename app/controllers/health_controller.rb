d4992dcd58d9f0eef29cff82d03860316945f8ed
pepeherr <pepeherr@gmail.com>
2026-06-05 20:16:08 +0200

    Cambio de Sidekiq::ProcessSet.new.size en lugar de stats.processes_count en app/controllers/health_controller.rb

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -3,8 +3,9 @@ class HealthController < ApplicationController
   def sidekiq
     stats = Sidekiq::Stats.new
     latency = Sidekiq::Queue.new.latency
+    processes = Sidekiq::ProcessSet.new.size
 
-    if stats.processes_count == 0
+    if processes == 0
       render json: { status: 'error', reason: 'No Sidekiq processes running' },
              status: :service_unavailable
     elsif latency > 300
@@ -13,7 +14,7 @@ class HealthController < ApplicationController
     else
       render json: {
         status: 'ok',
-        processes: stats.processes_count,
+        processes: processes,
         enqueued: stats.enqueued,
         failed: stats.failed,
         latency: latency.round(2)
