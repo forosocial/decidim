3f84cbb25de69c59a6bf0cc3b74003d1d4770d57
pepeherr <pepeherr@gmail.com>
2026-06-05 19:23:30 +0200

    Add Sidekiq health check endpoint- correct config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,11 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-
+  get "/health/sidekiq", to: "health#sidekiq"
   mount Decidim::Core::Engine => '/'
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  get "/health/sidekiq", to: "health#sidekiq"
+  
 end
