fdf8b399f7c02f9a59c186d79d9df80fe7b3963e
pepeherr <pepeherr@gmail.com>
2026-06-09 20:11:01 +0200

    modificación en config/initializers/amendments_helper_override.rb envolviendo el class_eval en to_prepare

--- config/initializers/amendments_helper_override.rb
+++ config/initializers/amendments_helper_override.rb
@@ -8,20 +8,25 @@
 # El override fuerza form.object[:body] al string traducido antes de que
 # hidden_field lo lea.
 
-Decidim::AmendmentsHelper.class_eval do
-  def amendments_form_field_for(attribute, form, original_resource)
-    options = {
-      label: amendments_form_fields_label(attribute),
-      value: amendments_form_fields_value(original_resource, attribute)
-    }
+Rails.application.config.to_prepare do
+  Decidim::AmendmentsHelper.class_eval do
+    def amendments_form_field_for(attribute, form, original_resource)
+      options = {
+        label: amendments_form_fields_label(attribute),
+        value: amendments_form_fields_value(original_resource, attribute)
+      }
 
-    case attribute
-    when :title
-      form.text_field(:title, options)
-    when :body
-      form.object[:body] = options[:value]
-      options.delete(:value)
-      text_editor_for(form, :body, options)
+      case attribute
+      when :title
+        form.text_field(:title, options)
+      when :body
+        # El body se almacena como Hash translatable. Forzamos strip_tags: false
+        # porque el editor TipTap necesita el HTML para inicializarse,
+        # independientemente de rich_text_editor_in_public_views.
+        form.object[:body] = present(send(original_resource)).body(strip_tags: false).strip
+        options.delete(:value)
+        text_editor_for(form, :body, options)
+      end
     end
   end
 end
