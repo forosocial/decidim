7406e6fb086273c236c4408bcdb223e168aad495
pepeherr <pepeherr@gmail.com>
2026-06-22 18:04:09 +0200

    modificados:     config/initializers/sidekiq.rb 	nuevos archivos: config/initializers/sidekiq.rb.bak Eliminada linea config.queues = %w[default mailers events exports]

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,13 @@
 # config/initializers/sidekiq.rb
-# Añadido por pepeherr
-# Gestión de colas
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0") }
-  config.queues = %w[default mailers events exports]
+  config.redis = {
+    url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0")
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0") }
+  config.redis = {
+    url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379/0")
+  }
 end
