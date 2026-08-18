98c27b1176b777bccaf78b09637a4eb7748840fe
pepeherr <pepeherr@gmail.com>
2026-08-18 11:20:41 +0200

    modificado:     config/initializers/decidim_image_validation.rb cambio de config_to

--- config/initializers/decidim_image_validation.rb
+++ config/initializers/decidim_image_validation.rb
@@ -1,22 +1,22 @@
 # config/initializers/decidim_image_validation.rb
-Rails.application.config.to_prepare do
+Rails.application.config.after_initialize do
   Decidim::Attachment.class_eval do
-    validates :file, image_content: true, if: -> { file.attached? && file.image? }
+    validates :file, image_content: true, if: proc { file.attached? && file.image? }
   end
 
   Decidim::User.class_eval do
-    validates :avatar, image_content: true, if: -> { avatar.attached? }
+    validates :avatar, image_content: true, if: proc { respond_to?(:avatar) && avatar.attached? }
   end
 
   Decidim::Organization.class_eval do
-    validates :logo, image_content: true, if: -> { logo.attached? }
-    validates :favicon, image_content: true, if: -> { favicon.attached? }
-    validates :official_img_footer, image_content: true, if: -> { official_img_footer.attached? }
+    validates :logo, image_content: true, if: proc { logo.attached? }
+    validates :favicon, image_content: true, if: proc { favicon.attached? }
+    validates :official_img_footer, image_content: true, if: proc { official_img_footer.attached? }
   end
 
   if defined?(Decidim::EditorImage)
     Decidim::EditorImage.class_eval do
-      validates :file, image_content: true, if: -> { file.attached? }
+      validates :file, image_content: true, if: proc { file.attached? }
     end
   end
 end
